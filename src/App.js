af3958d44e859ca4569a746f666c6d32101a6aaa
Jeongmin Oh <battlerhythm@gmail.com>
2018-07-09 22:56:04 -0400

    Learning Props

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
+const movieTitles = [
+  "Matrix",
+  "Full Metal Jacket",
+  "Oldboy",
+  "Star Wars"
+]
+
+const movieImages = [
+  "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMTUzNDY0NjY4Nl5BMl5BanBnXkFtZTgwNjY4MTQ0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg"
+
+]
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
+        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
+        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
+        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
+        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -3,11 +3,12 @@ import './Movie.css';
 
 class Movie extends Component{
   render(){
+    console.log(this.props);
     return(
       <div>
-        <MoviePoster />
-        <h1>Hello this is a movie</h1>
-      </div>  
+        <MoviePoster poster={this.props.poster} />
+        <h1>{this.props.title}</h1>
+      </div>
     )
   }
 }
@@ -15,7 +16,7 @@ class Movie extends Component{
 class MoviePoster extends Component{
   render(){
     return(
-      <img src="https://cdn.namuwikiusercontent.com/s/bb8ac0b78d3cfb5a369e1089bd02ff248cc93a6ba174a3f2fabf715fed7e9501b21b09919ae47dd8abd661b3daafdb70aea9587845b611e276e7a56df44905c4aaf9246a219446522aa6ce16c01cdcea?e=1531598756&k=lyb_2uc1Ol56su18WUPKmw" />
+      <img src={this.props.poster} />
     )
   }
 }
